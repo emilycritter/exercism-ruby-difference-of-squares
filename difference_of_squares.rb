6b46654cbc12069b81f6a6d25b97c1167bc6527f
emily.c.ritter <emily.c.ritter@gmail.com>
2016-06-13 21:55:26 -0500

    Clean up code. Alter methods to reduce amount of code

--- difference_of_squares.rb
+++ difference_of_squares.rb
@@ -1,26 +1,16 @@
 class Squares
   def initialize(num)
     @num = num
+    @array_of_num = (0..num).to_a
   end
 
   def square_of_sum
-    num = @num
-    total_sum = 0
-    while num > 0
-      total_sum += num
-      num -= 1
-    end
-    return total_sum**2
+    total_sum = @array_of_num.reduce(:+)
+    total_sum**2
   end
 
   def sum_of_squares
-    num = @num
-    total_of_squares = 0
-    while num > 0
-      total_of_squares += num**2
-      num -= 1
-    end
-    return total_of_squares
+    @array_of_num.map{|num| num**2}.reduce(:+)
   end
 
   def difference
